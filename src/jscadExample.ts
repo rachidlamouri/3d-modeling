22e89c4977a6a6e16867a18b8944222433a9755f
Rachid Lamouri <rachidlamouri@gmail.com>
2021-08-15 11:52:21 -0400

    Add typescript to project

--- src/jscadExample.ts
+++ src/jscadExample.ts
@@ -1,14 +1,15 @@
+import * as jscad from '@jscad/modeling';
+
 const {
   primitives: { cuboid },
   transforms: { translate },
   booleans: { subtract },
-} = require('@jscad/modeling');
-const main = () => {
+} = jscad;
+
+export const main = () => {
   const outerBox = cuboid({ size: [20, 40, 40] });
   const innerBox = translate([0, 0, 2], cuboid({ size: [18, 38, 38] }));
   const container = subtract(outerBox, innerBox);
 
   return container;
 }
-
-module.exports = { main }
