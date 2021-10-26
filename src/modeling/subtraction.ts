12f17fb3cb03d8f0f2842912c0bb8429498dfef7
Rachid Lamouri <rachidlamouri@gmail.com>
2021-10-25 20:28:19 -0400

    Update subtraction to accept models

--- src/modeling/subtraction.ts
+++ src/modeling/subtraction.ts
@@ -3,8 +3,15 @@ import { Vector3DObject } from './vector';
 import { Operation3D } from './operation3D';
 
 type SubtractionParams = {
+  models?: never;
   minuend: Model3D;
   subtrahends: Model3D[];
+  rotations?: RotationInput[];
+  translation?: Partial<Vector3DObject>;
+} | {
+  models: [Model3D, ...Model3D[]];
+  minuend?: never;
+  subtrahends?: never;
   rotations?: RotationInput[];
   translation?: Partial<Vector3DObject>;
 }
@@ -14,11 +21,24 @@ export class Subtraction extends Operation3D {
   subtrahends: Model3D[];
 
   constructor({
-    minuend,
-    subtrahends,
+    models,
+    minuend: inputMinuend,
+    subtrahends: inputSubtrahends,
     rotations = [],
     translation = {},
   }: SubtractionParams) {
+    if (models !== undefined && models.length === 0) {
+      throw Error('"models" cannot be empty');
+    }
+
+    const minuend = inputMinuend !== undefined
+      ? inputMinuend
+      : (models as Model3D[])[0];
+
+    const subtrahends = inputSubtrahends !== undefined
+      ? inputSubtrahends
+      : (models as Model3D[]).slice(1);
+
     super({
       position: minuend.position,
       rotations,
