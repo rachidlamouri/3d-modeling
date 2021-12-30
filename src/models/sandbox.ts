024ff076b9fd0533ef0a15b3af9bf2475ca96d34
Rachid Lamouri <rachidlamouri@gmail.com>
2021-12-30 16:32:39 -0500

    Add sandbox for ephemeral testing

---
+++ src/models/sandbox.ts
@@ -0,0 +1,10 @@
+import { RectangularPrism } from '../modeling';
+
+export default {
+  sandbox: new RectangularPrism({
+    origin: ['center', 'center', 'bottom'],
+    lengthX: 20,
+    lengthY: 16,
+    lengthZ: 10,
+  }),
+};
