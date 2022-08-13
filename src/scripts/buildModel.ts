d2da1dd97dc42ef8b47d56d44b6c73d6fce9f3a1
Rachid Lamouri <rachidlamouri@gmail.com>
2022-08-13 19:33:03 -0400

    Fix a bug with jscad cli not building two models at once
    Despite spawning two separate commands, the jscad cli would
    randomly repeat a previous command. Not sure what the root cause of the
    bug was.

--- src/scripts/buildModel.ts
+++ src/scripts/buildModel.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 import _ from 'lodash';
-import { exec } from 'child_process';
+import { spawn } from 'child_process';
 import fs from 'fs';
 
 const [src] = process.argv.slice(2);
@@ -20,20 +20,32 @@ if (!src || !/\.ts$/.test(src)) {
   process.exit(1);
 }
 
-import(`../../${src}`).then((models) => {
+import(`../../${src}`).then(async (models) => {
   if (!models.default || !_.isPlainObject(models.default) || Object.keys(models.default).length === 0) {
     console.log(`"${src}" does not export a default object with at least one key`);
     process.exit(1);
   }
 
   const modelNames = Object.keys(models.default);
-  modelNames.forEach((modelName) => {
-    const childProcess = exec(`npx jscad scripts/loadModel.js --filepath ${src} --model ${modelName} -o ${src.replace(/^src/, 'build/src').replace(/\.ts$/, `.${modelName}.stl`)}"`, {});
-    childProcess.stdout?.pipe(process.stdout);
-    childProcess.stderr?.pipe(process.stderr);
 
-    childProcess.on('exit', () => {
-      console.log('Finished:', modelName);
+  for (let i = 0; i < modelNames.length; i += 1) {
+    const modelName = modelNames[i];
+
+    const outputPath = src.replace(/^src/, 'build/src').replace(/\.ts$/, `.${modelName}.stl`);
+    const command = ['npx.cmd', 'jscad', 'scripts/loadModel.js', '--filepath', `${src}`, '--model', `${modelName}`, '-o', `${outputPath}`];
+
+    // eslint-disable-next-line no-await-in-loop
+    await new Promise<void>((resolve) => {
+      const childProcess = spawn(command[0], command.slice(1));
+      childProcess.stdout?.pipe(process.stdout);
+      childProcess.stderr?.pipe(process.stderr);
+
+      childProcess.on('exit', (exitCode) => {
+        const description = exitCode === 0 ? 'Finished:' : 'Failed:';
+
+        console.log(description, modelName);
+        resolve();
+      });
     });
-  });
+  }
 });
